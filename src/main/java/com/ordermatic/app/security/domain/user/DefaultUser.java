cc4d13e1e0f54a984e47c88d8b0caddce915f835
NicolasMirandannm <nicolasleonardonnm@gmail.com>
2024-07-31 20:49:37 -0400

    task-38: default user

---
+++ src/main/java/com/ordermatic/app/security/domain/user/DefaultUser.java
@@ -0,0 +1,16 @@
+package com.ordermatic.app.security.domain.user;
+
+import com.ordermatic.shared.ddd.AggregateRoot;
+import com.ordermatic.shared.ddd.DomainEntity;
+import com.ordermatic.shared.utilitaires.valueobjs.UniqueIdentifier;
+
+public class DefaultUser extends DomainEntity<DefaultUser> {
+  private String name;
+  private String email;
+  private String phone;
+  private String password;
+
+  protected DefaultUser(UniqueIdentifier id) {
+    super(id);
+  }
+}
