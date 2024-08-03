2bd407ac1ab556208adf4640e85173ea62e0c153
NicolasMirandannm <nicolasleonardonnm@gmail.com>
2024-08-03 08:58:23 -0400

    task-38: value objetcs to user created

---
+++ src/main/java/com/ordermatic/app/security/domain/user/CustumerUser.java
@@ -0,0 +1,14 @@
+package com.ordermatic.app.security.domain.user;
+
+import com.ordermatic.app.security.domain.user.valueobjects.Address;
+import com.ordermatic.app.security.domain.user.valueobjects.Cpf;
+import com.ordermatic.shared.utilitaires.valueobjs.UniqueIdentifier;
+
+public class CustumerUser extends DefaultUser {
+  private Address address;
+  private Cpf cpf;
+
+  public CustumerUser(UniqueIdentifier id) {
+    super(id);
+  }
+}
--- src/main/java/com/ordermatic/app/security/domain/user/DefaultUser.java
+++ src/main/java/com/ordermatic/app/security/domain/user/DefaultUser.java
@@ -1,13 +1,16 @@
 package com.ordermatic.app.security.domain.user;
 
+import com.ordermatic.app.security.domain.user.valueobjects.*;
 import com.ordermatic.shared.ddd.DomainEntity;
 import com.ordermatic.shared.utilitaires.valueobjs.UniqueIdentifier;
+import lombok.Getter;
 
-public class DefaultUser extends DomainEntity<DefaultUser> {
-  private String name;
-  private String email;
-  private String phone;
-  private String password;
+@Getter
+public abstract class DefaultUser extends DomainEntity<DefaultUser> {
+  protected String name;
+  protected Email email;
+  protected Phone phone;
+  protected Password password;
 
   protected DefaultUser(UniqueIdentifier id) {
     super(id);
