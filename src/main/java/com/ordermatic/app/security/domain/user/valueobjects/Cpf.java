120a8d278bc271a69d93b0ae93d8d77cb8954f3f
NicolasMirandannm <nicolasleonardonnm@gmail.com>
2024-08-03 14:59:39 -0400

    task-38: Cpf value object created with validations

--- src/main/java/com/ordermatic/app/security/domain/user/valueobjects/Cpf.java
+++ src/main/java/com/ordermatic/app/security/domain/user/valueobjects/Cpf.java
@@ -1,4 +1,57 @@
 package com.ordermatic.app.security.domain.user.valueobjects;
 
-public class Cpf {
+import com.ordermatic.shared.ddd.ValueObject;
+import com.ordermatic.shared.exceptions.DomainException;
+import lombok.Getter;
+import lombok.NonNull;
+
+@Getter
+public class Cpf extends ValueObject {
+
+  private final String value;
+
+  public Cpf() {
+    this.value = null;
+  }
+
+  public Cpf(@NonNull String value) {
+    this.value = getValidCpf(value);
+  }
+
+  private static String getCpfWithoutMask(String value) {
+    return value.replaceAll("[^0-9]", "");
+  }
+
+  private static String getValidCpf(String value) {
+    var cpf = getCpfWithoutMask(value);
+    if (cpf.length() != 11) {
+      throw new DomainException("Invalid amount of numeric characters for CPF.");
+    }
+    else if (allDigitsAreEqual(cpf) || !verifyCpfDigits(cpf)) {
+      throw new DomainException(cpf + " is a invalid CPF.");
+    }
+    return cpf;
+  }
+
+  private static boolean allDigitsAreEqual(String cpf) {
+    return cpf.matches("(\\d)\\1{10}");
+  }
+
+  private static boolean verifyCpfDigits(String cpf) {
+    int sum = 0;
+    for (int i = 0; i < 9; i++) {
+      sum += (cpf.charAt(i) - '0') * (10 - i);
+    }
+    int firstVerifierDigit = 11 - (sum % 11);
+    firstVerifierDigit = (firstVerifierDigit >= 10) ? 0 : firstVerifierDigit;
+
+    sum = 0;
+    for (int i = 0; i < 10; i++) {
+      sum += (cpf.charAt(i) - '0') * (11 - i);
+    }
+    int secondVerifierDigit = 11 - (sum % 11);
+    secondVerifierDigit = (secondVerifierDigit >= 10) ? 0 : secondVerifierDigit;
+
+    return cpf.charAt(9) - '0' == firstVerifierDigit && cpf.charAt(10) - '0' == secondVerifierDigit;
+  }
 }
--- src/main/java/com/ordermatic/shared/ddd/ValueObject.java
+++ src/main/java/com/ordermatic/shared/ddd/ValueObject.java
@@ -1,15 +1,18 @@
 package com.ordermatic.shared.ddd;
 
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
+import java.lang.reflect.Field;
 import java.util.Objects;
 
 @Getter
-public abstract class ValueObject<T> {
-  private final T value;
+@Slf4j
+public abstract class ValueObject {
+  private final String objectName;
 
-  protected ValueObject(T value) {
-    this.value = value;
+  protected ValueObject() {
+    this.objectName = this.getClass().getSimpleName();
   }
 
   @Override
@@ -18,12 +21,24 @@ public abstract class ValueObject<T> {
       return false;
     }
 
-    ValueObject<T> other = (ValueObject<T>) obj;
-
-    if (this.value == null) {
-      return other.getValue() == null;
+    Class<?> clazz = this.getClass();
+    while (clazz != null) {
+      Field[] fields = clazz.getDeclaredFields();
+      for (Field field : fields) {
+        field.setAccessible(true);
+        try {
+          Object thisValue = field.get(this);
+          Object otherValue = field.get(obj);
+          if (!Objects.equals(thisValue, otherValue)) {
+            return false;
+          }
+        } catch (IllegalAccessException e) {
+          log.error("Error while comparing {} value object", objectName, e);
+          return false;
+        }
+      }
+      clazz = clazz.getSuperclass();
     }
-
-    return Objects.equals(this.value, other.getValue());
+    return true;
   }
 }
