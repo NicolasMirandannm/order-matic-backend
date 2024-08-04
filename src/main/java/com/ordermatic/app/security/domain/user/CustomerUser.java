e85a7d72c4191860c317f6ed00f0723d8418b8d6
NicolasMirandannm <nicolasleonardonnm@gmail.com>
2024-08-04 19:40:01 -0400

    task-38: CustomerUser aggregate created.

--- src/main/java/com/ordermatic/app/security/domain/user/CustomerUser.java
+++ src/main/java/com/ordermatic/app/security/domain/user/CustomerUser.java
@@ -1,14 +1,16 @@
 package com.ordermatic.app.security.domain.user;
 
-import com.ordermatic.app.security.domain.user.valueobjects.address.Address;
 import com.ordermatic.app.security.domain.user.valueobjects.Cpf;
+import com.ordermatic.app.security.domain.user.valueobjects.address.Address;
 import com.ordermatic.shared.utilitaires.valueobjs.UniqueIdentifier;
+import lombok.Getter;
 
-public class CustumerUser extends DefaultUser {
+@Getter
+public class CustomerUser extends DefaultUserAggregate {
   private Address address;
   private Cpf cpf;
 
-  public CustumerUser(UniqueIdentifier id) {
+  public CustomerUser(UniqueIdentifier id) {
     super(id);
   }
 }
--- src/main/java/com/ordermatic/app/security/domain/user/DefaultUserAggregate.java
+++ src/main/java/com/ordermatic/app/security/domain/user/DefaultUserAggregate.java
@@ -1,18 +1,18 @@
 package com.ordermatic.app.security.domain.user;
 
 import com.ordermatic.app.security.domain.user.valueobjects.*;
-import com.ordermatic.shared.ddd.DomainEntity;
+import com.ordermatic.shared.ddd.AggregateRoot;
 import com.ordermatic.shared.utilitaires.valueobjs.UniqueIdentifier;
 import lombok.Getter;
 
 @Getter
-public abstract class DefaultUser extends DomainEntity<DefaultUser> {
+public abstract class DefaultUserAggregate extends AggregateRoot<DefaultUserAggregate> {
   protected String name;
   protected Email email;
   protected Phone phone;
   protected String password;
 
-  protected DefaultUser(UniqueIdentifier id) {
+  protected DefaultUserAggregate(UniqueIdentifier id) {
     super(id);
   }
 }
