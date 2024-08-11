91ccf9432d698fc6b376fada6920cc5ed056568b
NicolasMirandannm <nicolasleonardonnm@gmail.com>
2024-08-10 20:24:00 -0400

    task-44: migrations configuration with flyway

---
+++ src/main/resources/migrations/default/V1__create_custumer_user.sql
@@ -0,0 +1,11 @@
+create table customer_user (
+    id uuid primary key,
+    name varchar(150) not null,
+    password varchar(255) not null,
+    email varchar(255) not null,
+    phone varchar(11) not null,
+    created_at timestamp not null default now(),
+    updated_at timestamp not null default now(),
+
+    constraint unique_email unique(email)
+);
