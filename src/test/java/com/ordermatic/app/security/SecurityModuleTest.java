12bd97a509452a3aaa4a5d5887f63872b3f0a652
NicolasMirandannm <nicolasleonardonnm@gmail.com>
2024-08-18 16:35:06 -0400

    task-49: h2 database configuration

---
+++ src/test/java/com/ordermatic/app/security/SecurityModuleTest.java
@@ -0,0 +1,12 @@
+package com.ordermatic.app.security;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+public class SecurityModuleTest {
+}
