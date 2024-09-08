91fd9b3a1963d9f822973117180e25bf881d19f8
NicolasMirandannm <nicolasleonardonnm@gmail.com>
2024-09-08 16:03:52 -0400

    adjusts

--- src/main/java/com/ordermatic/app/security/infra/config/SecurityConfiguration.java
+++ src/main/java/com/ordermatic/app/security/infra/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.ordermatic.app.security.infra.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -7,19 +8,29 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+  @Value("${security.password.encoder.strength}")
+  private int strength;
+
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
     return httpSecurity
-      .csrf(AbstractHttpConfigurer::disable)
       .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
       .authorizeHttpRequests(authorize -> authorize
-        .requestMatchers(HttpMethod.POST, "/security/customer-user").permitAll())
+        .requestMatchers(HttpMethod.POST, "/security/customer-user").permitAll()
+        .anyRequest().authenticated())
       .build();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder(strength);
   }
 }
